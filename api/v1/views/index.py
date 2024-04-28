712b3889fa22739bd3350872df8d091a69659f65
Richie911 <oohrichmond.k@gmail.com>
2024-04-28 18:10:11 +0000

    task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+application view
+"""
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def api_status():
+    """return status OK"""
+    resp = {
+        'status': "OK"
+    }
+    return jsonify(resp)
