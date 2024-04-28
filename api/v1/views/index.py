8f9087da467f9e39b0b97331991539d69072a979
Richie911 <oohrichmond.k@gmail.com>
2024-04-28 18:43:40 +0000

    apis

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-application view
+application index
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
@@ -13,3 +14,23 @@ def api_status():
         'status': "OK"
     }
     return jsonify(resp)
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
